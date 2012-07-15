6f336ec77b5e5d737b305c5f946ed7e8b4e2fe76
wpcolor <wpcolor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-15 10:51:14 +0000

    git-svn-id: https://plugins.svn.wordpress.org/menu-master-custom-widget/tags/2.1@572542 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ menu-master-custom-widget.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+Plugin Name: Menu Master Custom Widget
+Plugin URI: http://1customize.com
+Description: A highly customizable sidebar menu with jquery effects
+Version: 2.1
+Author: 1customize
+*/
+
+
+
+/**************************************************************************
+
+Copyright (C) 2011-2012 1customize
+
+email : info@1customize.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License
+		
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  gnu.org/licenses
+
+**************************************************************************/
+@include_once('sidemenu.php');
+@include_once('lib.php');
+   
+?>
